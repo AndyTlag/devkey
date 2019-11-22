9528a5c1f5a5071d3765a2182a26ed7a4c1f7c43
Andy L <jadlog.andy@gmail.com>
2019-11-22 17:55:21 -0300

    adicionei bd novo

--- devkey.sql
+++ devkey.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: 31-Out-2019 às 21:14
+-- Generation Time: 22-Nov-2019 às 20:54
 -- Versão do servidor: 10.3.14-MariaDB
 -- versão do PHP: 7.2.18
 
@@ -40,25 +40,24 @@ CREATE TABLE IF NOT EXISTS `dev_tarefa` (
   `trf_ordem` int(11) DEFAULT NULL,
   PRIMARY KEY (`trf_id`),
   KEY `fk_trf_usu` (`trf_usu`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `dev_tarefa`
 --
 
 INSERT INTO `dev_tarefa` (`trf_id`, `trf_nome`, `trf_descricao`, `trf_usu`, `trf_data_ini`, `trf_data_fim`, `trf_status`, `trf_ordem`) VALUES
-(1, 'Documentação', NULL, 1, '2019-10-01', '2019-10-31', 'exe', 3),
-(2, 'Banner', NULL, 2, '2019-10-01', '2019-10-24', 'exe', 2),
-(3, 'Diagrama de Classe', NULL, 3, '2019-10-02', '2019-10-30', 'fin', 3),
-(4, 'Caso de Uso', 'Sistema ASP NET', 5, '2019-09-01', '2019-11-29', 'fin', 4),
-(5, 'oio', 'oio', 1, NULL, NULL, 'pen', 2),
+(3, 'Diagrama de Classe', '\r\n                                               aeeeeeeeeeeeeeeeeeeeeeee \r\n\r\n                                                ', 3, '2019-10-02', '2019-10-30', 'fin', 4),
+(4, 'Caso de Uso', 'Sistema ASP NET', 5, '2019-09-01', '2019-11-29', 'fin', 5),
+(5, 'oio', 'oio', 1, NULL, NULL, 'pen', 1),
 (6, 'tarefa', 'tarefa tarefada', 1, NULL, NULL, 'fin', 1),
 (7, 'eee', '        eee', 3, NULL, NULL, 'exe', 1),
 (8, 'aaaa', '        aaaaa', 4, NULL, NULL, 'pen', 3),
-(9, 'aaaa', '        aaaaa', 4, NULL, NULL, 'pen', 4),
-(10, 'qqqq', '        qqqq', 1, NULL, NULL, 'pen', 5),
-(11, 'oioioi', '        oioioioio', 7, NULL, NULL, 'pen', 1),
-(12, 'testeteste', '       hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ', 8, NULL, NULL, 'fin', 2);
+(9, 'aaaa', '        aaaaa', 4, NULL, NULL, 'pen', 5),
+(10, 'qqqq', '        qqqq', 1, NULL, NULL, 'pen', 2),
+(11, 'oioioi', '        oioioioio', 7, NULL, NULL, 'pen', 4),
+(12, 'testeteste', '       hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ', 8, NULL, NULL, 'exe', 2),
+(13, 'aqaqa', '\r\n                            aqaqaq            ', 1, NULL, NULL, 'fin', 3);
 
 -- --------------------------------------------------------
 
@@ -72,21 +71,22 @@ CREATE TABLE IF NOT EXISTS `dev_usuario` (
   `usu_nome` varchar(20) DEFAULT NULL,
   `usu_email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`usu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `dev_usuario`
 --
 
 INSERT INTO `dev_usuario` (`usu_id`, `usu_nome`, `usu_email`) VALUES
-(1, 'andy', 'andy@teste.com'),
+(1, 'andy-baca', 'andy@teste.com'),
 (2, 'andy2', 'andy2@teste.com'),
 (3, 'andy3', 'andy3@teste.com'),
 (4, 'andy4', 'andy4@teste.com'),
 (5, 'andy5', 'andy5@teste.com'),
 (6, 'hdhd', 'teste@teste.com'),
 (7, 'qqqqqqqqqqqqqq', 'qqqqqqqqqqqqqqq@ddd.com'),
-(8, 'teste', 'teste@nn.com');
+(8, 'teste', 'teste@nn.com'),
+(10, 'Andressa', 'andilima.oliveira@gmail.com');
 
 --
 -- Constraints for dumped tables
